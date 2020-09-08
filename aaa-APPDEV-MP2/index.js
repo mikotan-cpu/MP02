c75db54abe3ec4c34765eceaafc5214a05abd232
Joseph Lee <50176359+JLSL20@users.noreply.github.com>
2020-09-08 23:59:51 +0800

    initial commit

--- aaa-APPDEV-MP2/index.js
+++ aaa-APPDEV-MP2/index.js
@@ -1,52 +1,10 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const session = require("express-session");
 
-// const app = express();
-
-// const urlencoder = bodyParser.urlencoded({
-//   extended: false,
-// });
-// app.use(
-//   session({
-//     secret: "very secret",
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       maxAge: 1000 * 60 * 3,
-//     },
-//   })
-// );
-
-// app.get("/register", function (req, res) {
-//   if (req.session.username) {
-//     res.sendFile(__dirname + "/html-pages/home.html");
-//   } else {
-//     res.sendFile(__dirname + "/html-pages/register.html");
-//   }
-// });
-
-// app.post("/register", urlencoder, function (req, res) {
-//   req.session.username = req.body.un;
-//   res.sendFile(__dirname + "/html-pages/home.html");
-// });
-
-// app.post("/login", urlencoder, function (req, res) {
-//   req.session.username = req.body.un;
-
-//   res.sendFile(__dirname + "/html-pages/home.html");
-// });
-// app.use(express.static(__dirname));
-
-// app.listen(3000, function () {
-//   console.log("now listening to port 3000");
-//   console.log(__dirname);
-// });
 
 const express = require("express");
 const session = require("express-session");
 const bodyparser = require("body-parser");
 const cookieparser = require("cookie-parser");
+const { Router } = require("express");
 
 const app = express();
 
@@ -55,7 +13,11 @@ const urlencoder = bodyparser.urlencoded({
 });
 
 let users = [];
-
+app.use(
+  bodyparser.urlencoded({
+    extended: false,
+  })
+);
 app.use(
   session({
     secret: "very secret",
@@ -67,37 +29,35 @@ app.use(
   })
 );
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname + "/public"));
 
-
-app.get("/", (req, res)=>{
-  if(req.session.username){
-      //it means that user has already signed in
-      //go to home.html
-      res.render(__dirname+"/views/views/html-pages/loggedin-index.hbs")
-
-  }else{
-       //the user has not logged in
-      //go to index.html
-      res.sendFile(__dirname+"/views/views/html-pages/index.html",{ //palitan yung home.hbs with homepage natin basically index html
-        username:req.session.username
-    })
+app.get("/", (req, res) => {
+  console.log("/");
+  if (req.session.username) {
+    console.log("/2");
+    //it means that user has already signed in
+    //go to home.html
+    res.render("loggedin-index.hbs");
+  } else {
+    console.log("/3");
+    //the user has not logged in
+    //go to index.html
+    res.sendFile(__dirname + "/public/index.html");
   }
-})
-
+});
 
 app.get("/register", (req, res) => {
   if (req.session.username) {
     //it means that user has already signed in
     //go to home.html
-    res.render(__dirname + "/views/views/html-pages/loggedin-index.hbs", {
+    res.render("loggedin-index.hbs", {
       //palitan yung home.hbs with homepage natin basically index html
       username: req.session.username,
     });
   } else {
     //the user has not logged in
     //go to index.html
-    res.render(__dirname + "/views/views/html-pages/register.hbs");
+    res.render("register.hbs");
   }
 });
 
@@ -106,11 +66,11 @@ app.post("/register", urlencoder, (req, res) => {
   let password = req.body.pw;
 
   if (username.trim() == "" || password.trim() == "") {
-    res.render("views/html-pages/register.hbs", {
+    res.render("register.hbs", {
       error: "Enter a username and password",
     });
   } else if (!isAvailable(username)) {
-    res.render("views/html-pages/register.hbs", {
+    res.render("register.hbs", {
       error: "Username not available",
     });
   } else {
@@ -125,22 +85,22 @@ app.post("/register", urlencoder, (req, res) => {
       //para lang toh macheck yung laman ng user array hehe everytime may nag reregister
       console.log(users[i].username);
     }
-    res.render(__dirname + "/views/views/html-pages/loggedin-index.hbs");
+    res.render("loggedin-index.hbs");
   }
 });
 
 app.get("/login", (req, res) => {
   if (req.session.username) {
-    //it means that user has already signed in
+    //it means that user has already signepad in
     //go to home.html
-    res.render(__dirname + "/views/views/html-pages/loggedin-index.hbs", {
+    res.render("loggedin-index.hbs", {
       //palitan yung home.hbs with homepage natin basically index html
       username: req.session.username,
     });
   } else {
     //the user has not logged in
     //go to index.html
-    res.render(__dirname + "/views/views/html-pages/login-page2.hbs");
+    res.render("login-page2.hbs");
   }
 });
 app.post("/login", urlencoder, (req, res) => {
@@ -148,16 +108,20 @@ app.post("/login", urlencoder, (req, res) => {
   req.session.password = req.body.pw;
 
   if (!matches(req.session.username, req.session.password)) {
-    res.render("views/html-pages/login-page2.hbs", {
+    res.render("login-page2.hbs", {
       error: "Username and password does not match", //ilalagay toh as {{error}} dun sa login-page2.js
     });
   } else {
-    res.render(__dirname + "/views/views/html-pages/loggedin-index.hbs", {
+    res.render("loggedin-index.hbs", {
       //palitan yung home.hbs with homepage natin basically index html
       username: req.session.username,
     });
   }
 });
+
+
+
+app.use(express.static(__dirname));
 
 function isAvailable(username) {
   for (let i = 0; i < users.length; i++) {
@@ -181,12 +145,17 @@ app.get("/signout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
+
+// let userInfo = require(__dirname + "/assets/js/symptoms.js");
+// console.log(userInfo);
+
+app.post("/getArray", (req, res) => {
+  console.log("POST /getArray");
+  let symptoms = req.body.symptoms;
+  console.log("symptoms: " + JSON.stringify(req.body));
+  res.send(JSON.stringify(symptoms));
+});
+
 app.listen(3000, function () {
   console.log("now listening to port 30000");
 });
-
-var array
-app.post("/getArray", (req, res)=>{
-  array = req.body.arr
-  console.log("index.js " + array)
-})
---
+++ aaa-APPDEV-MP2/public/js/geomap.js
@@ -0,0 +1,603 @@
+var x = document.getElementById("demo");
+var vlat;
+var vlong;
+
+
+
+function getLocation() {
+if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(showPosition);
+  
+} else { 
+ x.innerHTML = "Geolocation is not supported by this browser.";
+}
+}
+
+function showPosition(position) {
+x.innerHTML = "Latitude: " + position.coords.latitude + 
+"<br>Longitude: " + position.coords.longitude;
+ 
+ vlat =  position.coords.latitude;
+ vlong = position.coords.longitude;
+ console.log(vlat + vlong)
+
+ initMap()
+}
+
+ 
+   var map;
+   function initMap() {
+     map = new google.maps.Map(
+         document.getElementById('map'),
+         {center: new google.maps.LatLng(14.6037, 121.3084), zoom: 8});
+
+     var iconBase =
+         'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+
+    var icons = {
+      parking: {
+       icon: 'marker.png' 
+      },
+       confirmed: {
+        icon: '/../assets/images/confirmed.png'  
+       },
+       suspect: {
+         icon: '/../assets/images/suspect.png' 
+       },
+       probable: {
+         icon: '/../assets/images/probable.png' 
+       },
+       recovered: {
+         icon: '/../assets/images/recovered.png' 
+       },
+       noSymptoms: {
+         icon: '/../assets/images/nosymps.png' 
+       
+     }
+     };
+
+       var alati = 16.131400;
+       var alongi = 124.858482;
+   let markers = [ 
+     {
+         position: new google.maps.LatLng(vlat, vlong),
+         type: 'confirmed' 
+     },
+     
+     {
+      
+       position: new google.maps.LatLng(8.477217, 124.645920),
+         type: 'suspect'
+     }
+       ,
+     {
+       type: 'probable',
+       position: new google.maps.LatLng(10.753794, 123.084160)
+     },
+     {
+       type: 'probable',
+       position: new google.maps.LatLng(14.558555, 121.136086)
+     },
+     {
+       type: 'noSymptoms',
+         position: new google.maps.LatLng(10.503405, 122.966301)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(4.998451, 120.953209)
+     },
+     {
+       type:'probable',
+        position: new google.maps.LatLng(	6.116243, 125.171738)
+     },
+     {
+       type:'probable',
+        position: new google.maps.LatLng(	14.247142, 121.136673)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(	14.309243, 121.031708)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(	14.192048, 121.131180)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(		14.537752, 121.001381)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(		14.115286, 120.962112)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(		13.941876, 121.164421)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(		13.941876, 121.164421)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(			14.083507, 121.147354)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(			10.266182, 123.997292)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(			14.625483, 121.124481)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(				8.130604, 125.127655)
+     },
+     {
+       type:'confirmed',
+        position: new google.maps.LatLng(				7.056601, 124.676239)
+     },
+     {
+       type:'confirmed',
+        position: new google.maps.LatLng(				14.408133, 121.041466)
+     },
+     {
+       type:'confirmed',
+        position: new google.maps.LatLng(				15.090024, 120.662842)
+     },
+     {
+       type:'confirmed',
+        position: new google.maps.LatLng(				14.676208	, 121.043861)
+     },
+     {
+       type:'suspect',
+        position: new google.maps.LatLng(				15.156311	, 120.591774)
+     },
+     {
+       type:'probable',
+        position: new google.maps.LatLng(					9.740696	, 118.730072)
+     },
+     {
+       type:'confirmed',
+        position: new google.maps.LatLng(					16.0832	, 120.6200)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(					17.6132	, 121.7270)
+     },
+     {
+       type:'noSymptoms',
+        position: new google.maps.LatLng(					17.5705	, 120.3873)
+     },
+     {
+       type:'recovered',
+        position: new google.maps.LatLng(					17.0981	, 120.9073)
+     }
+   ]  
+     
+  
+
+     // Create markers.
+   console.log("array size: " + markers.length)
+     for (var i = 0; i < markers.length; i++) 
+     {
+       var marker = new google.maps.Marker({
+         position: markers[i].position,
+         icon: icons[markers[i].type].icon,
+         map: map
+          });
+      };
+   }
+ 
+   var filteredStatus = []
+   var unfilteredStatus = []
+
+   $('input[type=checkbox]').on('change', function() {
+    if($(this).is(':checked'))
+    { let filter =  $(this).val()
+        filteredStatus.push(filter)
+        for(var i = unfilteredStatus.length - 1; i >= 0; i--) {
+          if(unfilteredStatus[i] == filter) {
+              unfilteredStatus.splice(i, 1);
+          }
+      }
+        filterMap()
+}});
+
+$('input[type=checkbox]').on('change', function() {
+  if(!$(this).is(':checked'))
+    { let filter =  $(this).val()
+        unfilteredStatus.push(filter)
+        // filteredStatus = filteredStatus.filter(filter)
+        for(var i = filteredStatus.length - 1; i >= 0; i--) {
+          if(filteredStatus[i] == filter) {
+              filteredStatus.splice(i, 1);
+          }
+      }
+        unfilterMap()
+}
+});
+
+function filterMap() {
+  map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: new google.maps.LatLng(14.6037, 121.3084), zoom: 8});
+      console.log("FILTER() FILTERED")
+     
+ var icons = {
+   parking: {
+    icon: 'marker.png' 
+   },
+   confirmed: {
+    icon: '/../assets/images/confirmed.png'  
+   },
+   suspect: {
+     icon: '/../assets/images/suspect.png' 
+   },
+   probable: {
+     icon: '/../assets/images/probable.png' 
+   },
+   recovered: {
+     icon: '/../assets/images/recovered.png' 
+   },
+   noSymptoms: {
+     icon: '/../assets/images/nosymps.png' 
+    
+  }
+  };
+
+let markers = [ 
+  {
+      position: new google.maps.LatLng(vlat, vlong),
+      type: 'confirmed' 
+  },
+  {
+     position: new google.maps.LatLng(8.477217, 124.645920),
+      type: 'suspect'
+  }
+    ,
+  {
+    type: 'probable',
+    position: new google.maps.LatLng(10.753794, 123.084160)
+  },
+  {
+    type: 'probable',
+    position: new google.maps.LatLng(14.558555, 121.136086)
+  },
+  {
+    type: 'noSymptoms',
+      position: new google.maps.LatLng(10.503405, 122.966301)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(4.998451, 120.953209)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(	6.116243, 125.171738)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(	14.247142, 121.136673)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(	14.309243, 121.031708)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(	14.192048, 121.131180)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(		14.537752, 121.001381)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		14.115286, 120.962112)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		13.941876, 121.164421)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		13.941876, 121.164421)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(			14.083507, 121.147354)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(			10.266182, 123.997292)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(			14.625483, 121.124481)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(				8.130604, 125.127655)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				7.056601, 124.676239)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				14.408133, 121.041466)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				15.090024, 120.662842)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				14.676208	, 121.043861)
+  },
+  {
+    type:'suspect',
+     position: new google.maps.LatLng(				15.156311	, 120.591774)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(					9.740696	, 118.730072)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(					16.0832	, 120.6200)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(					17.6132	, 121.7270)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(					17.5705	, 120.3873)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(					17.0981	, 120.9073)
+  }
+]  
+  
+  // Create markers.
+
+var gmarkers = []
+for (var i = 0; i < markers.length; i++) 
+{
+   var marker = new google.maps.Marker({
+    position: markers[i].position,
+    icon: icons[markers[i].type].icon,
+    category: markers[i].type,
+    map: map
+    }); 
+    gmarkers.push(marker)
+}
+
+
+for(var j = 0; j < filteredStatus.length; j++) 
+{
+   
+  for (var i = 0; i < gmarkers.length; i++) 
+    {
+      
+      if (filteredStatus[j] == gmarkers[i].category )
+       { 
+        gmarkers[i].setVisible(false)
+        
+       }
+       
+    }
+    
+ }
+ for(var j = 0; j < unfilteredStatus.length; j++) 
+{
+   
+  for (var i = 0; i < gmarkers.length; i++) 
+    {
+      
+      if (unfilteredStatus[j] == gmarkers[i].category )
+       { 
+        gmarkers[i].setVisible(true)
+        
+       }
+       
+    }
+    
+  }
+  
+}
+
+function unfilterMap() {
+  map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: new google.maps.LatLng(14.6037, 121.3084), zoom: 8});
+
+ var icons = {
+   parking: {
+    icon: 'marker.png' 
+   },
+   confirmed: {
+    icon: '/../assets/images/confirmed.png'  
+   },
+   suspect: {
+     icon: '/../assets/images/suspect.png' 
+   },
+   probable: {
+     icon: '/../assets/images/probable.png' 
+   },
+   recovered: {
+     icon: '/../assets/images/recovered.png' 
+   },
+   noSymptoms: {
+     icon: '/../assets/images/nosymps.png' 
+    
+  }
+  };
+
+let markers = [ 
+  {
+      position: new google.maps.LatLng(vlat, vlong),
+      type: 'confirmed' 
+  },
+  {
+     position: new google.maps.LatLng(8.477217, 124.645920),
+      type: 'suspect'
+  }
+    ,
+  {
+    type: 'probable',
+    position: new google.maps.LatLng(10.753794, 123.084160)
+  },
+  {
+    type: 'probable',
+    position: new google.maps.LatLng(14.558555, 121.136086)
+  },
+  {
+    type: 'noSymptoms',
+      position: new google.maps.LatLng(10.503405, 122.966301)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(4.998451, 120.953209)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(	6.116243, 125.171738)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(	14.247142, 121.136673)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(	14.309243, 121.031708)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(	14.192048, 121.131180)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(		14.537752, 121.001381)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		14.115286, 120.962112)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		13.941876, 121.164421)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(		13.941876, 121.164421)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(			14.083507, 121.147354)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(			10.266182, 123.997292)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(			14.625483, 121.124481)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(				8.130604, 125.127655)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				7.056601, 124.676239)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				14.408133, 121.041466)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				15.090024, 120.662842)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(				14.676208	, 121.043861)
+  },
+  {
+    type:'suspect',
+     position: new google.maps.LatLng(				15.156311	, 120.591774)
+  },
+  {
+    type:'probable',
+     position: new google.maps.LatLng(					9.740696	, 118.730072)
+  },
+  {
+    type:'confirmed',
+     position: new google.maps.LatLng(					16.0832	, 120.6200)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(					17.6132	, 121.7270)
+  },
+  {
+    type:'noSymptoms',
+     position: new google.maps.LatLng(					17.5705	, 120.3873)
+  },
+  {
+    type:'recovered',
+     position: new google.maps.LatLng(					17.0981	, 120.9073)
+  }
+]  
+  
+  // Create markers.
+
+var gmarkers = []
+for (var i = 0; i < markers.length; i++) 
+{
+   var marker = new google.maps.Marker({
+    position: markers[i].position,
+    icon: icons[markers[i].type].icon,
+    category: markers[i].type,
+    map: map
+    }); 
+    gmarkers.push(marker)
+}
+
+
+for(var j = 0; j < unfilteredStatus.length; j++) 
+{
+   
+  for (var i = 0; i < gmarkers.length; i++) 
+    {
+      
+      if (unfilteredStatus[j] == gmarkers[i].category )
+       { 
+        gmarkers[i].setVisible(true)
+        
+       }
+       
+    }
+    
+  }
+  for(var j = 0; j < filteredStatus.length; j++) 
+{
+   
+  for (var i = 0; i < gmarkers.length; i++) 
+    {
+      
+      if (filteredStatus[j] == gmarkers[i].category )
+       { 
+        gmarkers[i].setVisible(false)
+        
+       }
+       
+    }
+    
+  }
+  // filterMap()
+}
